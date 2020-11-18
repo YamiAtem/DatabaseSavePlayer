b387cc51bbfd398eff9a1902ac7e7984b27c993d
Atharva Mishra <atharvamishra956@gmail.com>
2020-11-18 20:00:27 +0530

    added main functions and created canvas and added some variables

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var database;
+
+var playerCount;
+
+var player, form, game;
+
+function setup() {
+    createCanvas(500, 500);
+
+    database = firebase.database();
+}
+
+function draw() {
+    
+}
